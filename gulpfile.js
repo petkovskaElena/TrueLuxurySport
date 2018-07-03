db75c22a8096cc2f62217e4f55134372afa4e332
Elena Petkovska <elenap@sopro.io>
2018-07-03 15:14:51 +0200

    Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('default', function() {
-    console.log('Hello World');
+   
 });
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: './'
+      },
+    })
+  });
 
 gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('build/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
 
-});
+  gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('src/scss/**/*.scss', ['sass']); 
+    gulp.watch('./css/*.css', browserSync.reload);
+    gulp.watch('./*.html', browserSync.reload);
+  });
